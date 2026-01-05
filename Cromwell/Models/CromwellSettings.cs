bb514a35cf66c2901ce0b29ef8de79ff74acf8c5
gubard <maksimovsergey08@gmail.com>
2026-01-05 14:28:04 +0200

    Make `CromwellSettings` a sealed class and remove unused `Id` property.

--- Cromwell/Models/CromwellSettings.cs
+++ Cromwell/Models/CromwellSettings.cs
@@ -1,13 +1,9 @@
-﻿using Nestor.Db.Models;
+﻿namespace Cromwell.Models;
 
-namespace Cromwell.Models;
-
-[SourceEntity(nameof(Id))]
-public partial class CromwellSettings
+public sealed class CromwellSettings
 {
     private static readonly string DefaultGeneralKey = Guid.NewGuid().ToString().ToUpper();
 
-    public Guid Id { get; set; }
     public string GeneralKey { get; set; } = DefaultGeneralKey;
     public ThemeVariantType Theme { get; set; }
 }
