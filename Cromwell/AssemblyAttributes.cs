1d830b37451f204b44d93c48595351f37051429e
gubard <maksimovsergey08@gmail.com>
2026-01-21 15:15:59 +0200

    Remove obsolete assembly attributes from Cromwell project

--- Cromwell/AssemblyAttributes.cs
+++
@@ -1,9 +0,0 @@
-using Cromwell.Ui;
-using Inanna.Generator;
-
-[assembly: ViewPair(typeof(CredentialHeaderView), typeof(CredentialHeaderViewModel))]
-[assembly: ViewPair(typeof(CredentialView), typeof(CredentialViewModel))]
-[assembly: ViewPair(typeof(RootCredentialsView), typeof(RootCredentialsViewModel))]
-[assembly: ViewPair(typeof(CredentialParametersView), typeof(CredentialParametersViewModel))]
-[assembly: ViewPair(typeof(EditCredentialView), typeof(EditCredentialViewModel))]
-[assembly: ViewPair(typeof(RootCredentialsHeaderView), typeof(RootCredentialsHeaderViewModel))]
